bd1d27741acad16ed4b1d02eef50ad93d0e8ba72
yora132 <yarasalah132@gmail.com>
2023-05-19 07:01:26 +0000

    done

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     def e_r(elm):
-        return (elm if elm != search or replace)
+        return (elm if elm != search else replace)
     return list(map(e_r, my_list))
