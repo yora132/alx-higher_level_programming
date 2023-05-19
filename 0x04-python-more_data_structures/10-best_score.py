570bd9a522e751775c45567bedf3c2fabbc31267
yora132 <yarasalah132@gmail.com>
2023-05-19 06:36:02 +0000

    done

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(my_dict):
     if my_dict and len(my_dict):
-        max = list(my_dict.key())[0]
+        max = list(my_dict.keys())[0]
         for key in my_dict:
             if my_dict[key] > my_dict[max]:
                 max = key
